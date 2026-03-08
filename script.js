ab03cfd69245059fe8c7cf83d7162aabebdbc95c
Ruchivashisht001 <ruchigour104@gmail.com>
2026-03-08 16:49:14 +0530

    Portfolio website

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(){
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+window.addEventListener("scroll", function (){
+    let nav=document.querySelector("nav");
+    if (window.scrollY>400){
+        nav.style.background="black";
+    } else {
+        nav.style.background="transparent";
+    }
+});
+});
+// document.querySelector("form").addEventListener("submit", function(e){
+//     e.preventDefault();
+//     alert("Message Sent Successfully!");
+// });
